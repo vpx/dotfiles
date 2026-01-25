eaaab5ae536314850b325cd1a3803d4b002a52e1
Emanuele Milani <emanuele.milani06@gmail.com>
2026-01-25 22:44:47 +0100

    Configure vimtex

--- nvim/lua/configs/vimtex.lua
+++ nvim/lua/configs/vimtex.lua
@@ -1,7 +1,15 @@
-vim.g.vimtex_view_method = "zathura"
+local g = vim.g
+
+g.vimtex_view_method = 'zathura'
+g.vimtex_compiler_method = 'latexmk'
+g.vimtex_quickfix_mode = 0
+g.vimtex_complete_enabled = 1
+g.vimtex_complete_close_braces = 1
+g.vimtex_syntax_enabled = 1
 
 vim.g.vimtex_compiler_latexmk = {
     aux_dir = vim.fn.getcwd() .. "/build",
     out_dir = vim.fn.getcwd() .. "/build",
 }
 
+return g;
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -48,8 +48,8 @@ return {
     },
     {
       "lervag/vimtex",
-      lazy = false,     -- we don't want to lazy load VimTeX
-      -- tag = "v2.15", -- uncomment to pin to a specific release
+      ft = { "tex" },
+      lazy = false,
       init = function()
         local config = require("configs/vimtex")
       end
