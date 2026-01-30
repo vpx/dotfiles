d0629e48fff0627fb2fa9652446d44ee427a162e
Emanuele Milani <emanuele.milani06@gmail.com>
2026-01-30 20:36:42 +0100

    Install fish

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ packages=(
     "wl-clipboard"
     "wlr-randr"
     "xdg-desktop-portal-hyprland"
+    "fish"
     "zathura"
     "zathura-pdf-mupdf"
     "texlive-bin"
@@ -46,10 +47,16 @@ if ! command -v yay &> /dev/null; then
     cd .. && rm -rf yay
 fi
 
+# Remove hyprland config directory
+rm -r .config/hypr/
+
 # Temporary install stow, use it, then remove it
 sudo pacman -S stow --noconfirm
 stow .
 sudo pacman -Rns stow --noconfirm
+
+# Change the shell to fish
+chsh -s ${which fish}
 
 # Ask the user if they want to store git credentials
 read -p "Do you want to store your Git credentials permanently? (y/n): " response
